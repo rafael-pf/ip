5ca0e866255f593f80c6663a9d2b3ecfd3422ce5
rafael-pf <132072483+rafael-pf@users.noreply.github.com>
2023-08-10 16:25:22 -0300

    Update pokemon.c

--- questoes/pokemon.c
+++ questoes/pokemon.c
@@ -14,83 +14,10 @@ typedef struct{
     int nPokemons, pontuacao;
 } Treinador;
 
-Treinador *addTreinador(Treinador *treinadores, int *qtdCompetidores){
-    Treinador *aux;
-    aux = treinadores;
-    treinadores = (Treinador*) realloc(aux, (*qtdCompetidores+1)*sizeof(Treinador));
-    if(treinadores == NULL){
-        printf("Problema de alocacao!\n");
-        free(treinadores);
-        exit(1);
-    }
-    printf("Digite o nome do treinador: \n");
-    scanf(" %s", treinadores[*qtdCompetidores].nome);
-    (*qtdCompetidores)++;
-    treinadores[*qtdCompetidores].nPokemons=0;
-    treinadores[*qtdCompetidores].pontuacao=0;
-    printf("Treinador adicionado com sucesso!\n");
-    return treinadores;
-}
-
-Treinador *addPokemon(Treinador *treinadores, char *nomeTreinador, Pokemon pokemon, int qtdCompetidores){
-    int i=0;
-    Pokemon *aux;
-    for(i=0; i<qtdCompetidores; i++){
-        if(strcmp(nomeTreinador, treinadores[i].nome)==0){
-            aux = treinadores[i].pokebola;
-            treinadores[i].pokebola = (Pokemon*) realloc(aux, (treinadores[i].nPokemons+1)*sizeof(Pokemon));
-            if(treinadores[i].pokebola == NULL){
-                printf("Problema de alocacao!\n");
-                for(i=0; i<qtdCompetidores; i++){
-                    free(treinadores[i].pokebola);
-                }
-                free(treinadores);
-                exit(1);
-            }
-            treinadores[i].pokebola[treinadores[i].nPokemons] = pokemon;
-            treinadores[i].nPokemons++;
-        }
-    }
-    printf("Pokemon Adicionado com sucesso!\n");
-    return treinadores;
-}
-
-Treinador *removeTreinador(Treinador *treinadores, int *qtdCompetidores, int indexTreinador){
-
-}
-
-char *batalha(Treinador **treinadores, char *nomeCompetidor1, char *nomeCompetidor2, int indexPokemon1, int indexPokemon2, int *qtdCompetidores){
-    int iCompet1, vantCompet1;
-    int iCompet2, vantCompet2;
-    int i=0;
-    for(i=0; i<*qtdCompetidores; i++){
-        if(strcmp((*treinadores)[i].nome, nomeCompetidor1)==0){
-            iCompet1 = i;
-        }
-        else if(strcmp((*treinadores)[i].nome, nomeCompetidor1)==0){
-            iCompet2 = i;
-        }
-    }
-
-    vantCompet1 = (*treinadores)[iCompet1].pokebola[indexPokemon1].ataque - (*treinadores)[iCompet2].pokebola[indexPokemon2].defesa;
-    vantCompet2 = (*treinadores)[iCompet2].pokebola[indexPokemon2].ataque - (*treinadores)[iCompet1].pokebola[indexPokemon1].defesa;
-    if(vantCompet1 > vantCompet2){
-        (*treinadores)[iCompet1].pontuacao++;
-        (*treinadores)[iCompet2].pontuacao--;
-        if((*treinadores)[iCompet2].pontuacao==-5){
-            (*treinadores) = removeTreinador(*treinadores, &qtdCompetidores, iCompet2);
-        }
-        return (*treinadores)[iCompet1].nome;
-    }
-    else if(vantCompet2 > vantCompet1){
-        (*treinadores)[iCompet2].pontuacao++;
-        (*treinadores)[iCompet1].pontuacao--;
-        if((*treinadores)[iCompet1].pontuacao==-5){
-            (*treinadores) = removeTreinador(*treinadores, &qtdCompetidores, iCompet1);
-        }
-        return (*treinadores)[iCompet2].nome;
-    }
-}
+Treinador *addTreinador(Treinador *treinadores, int *qtdCompetidores);
+Treinador *addPokemon(Treinador *treinadores, char *nomeTreinador, Pokemon pokemon, int qtdCompetidores);
+Treinador *removeTreinador(Treinador *treinadores, int *qtdCompetidores, int indexTreinador);
+char *batalha(Treinador **treinadores, char *nomeCompetidor1, char *nomeCompetidor2, int indexPokemon1, int indexPokemon2, int *qtdCompetidores);
 
 int main(){
     int comando;
@@ -133,11 +60,11 @@ int main(){
             scanf(" %s", compet1);
             printf("Digite o nome do competidor 2\n");
             scanf(" %s", compet2);
-            printf("Digite o indice do pokemon de %s", compet1);
+            printf("Digite o indice do pokemon de %s\n", compet1);
             scanf("%d", &indPoke1);
-            printf("Digite o indice do pokemon de %s", compet2);
+            printf("Digite o indice do pokemon de %s\n", compet2);
             scanf("%d", &indPoke2);
-            printf("Vencedor: %s", batalha(&treinadores, compet1, compet2, indPoke1, indPoke2, &qtdTreinadores));
+            printf("->Vencedor: %s", batalha(&treinadores, compet1, compet2, indPoke1, indPoke2, &qtdTreinadores));
             break;
         case 4:
             printf("Saindo...");
@@ -149,5 +76,129 @@ int main(){
         printf("\n");
     }while(comando!=4);
 
+
+    for(int i=0; i<qtdTreinadores; i++){
+        free(treinadores[i].pokebola);
+    }
+    free(treinadores);
     return 0;
+}
+
+Treinador *addTreinador(Treinador *treinadores, int *qtdCompetidores){
+    Treinador *aux;
+    aux = treinadores;
+    treinadores = (Treinador*) realloc(aux, (*qtdCompetidores+1)*sizeof(Treinador));
+    if(treinadores == NULL){
+        printf("Problema de alocacao!\n");
+        for(int i=0; i<*qtdCompetidores; i++){
+            free(treinadores[i].pokebola);
+        }
+        free(treinadores);
+        exit(1);
+    }
+    printf("Digite o nome do treinador: \n");
+    scanf(" %s", treinadores[*qtdCompetidores].nome);
+    (*qtdCompetidores)++;
+    treinadores[*qtdCompetidores].nPokemons=0;
+    treinadores[*qtdCompetidores].pontuacao=0;
+    printf("Treinador adicionado com sucesso!\n");
+    return treinadores;
+}
+
+Treinador *addPokemon(Treinador *treinadores, char *nomeTreinador, Pokemon pokemon, int qtdCompetidores){
+    int i=0;
+    Pokemon *aux;
+    int tag_achei=0;
+    for(i=0; i<qtdCompetidores; i++){
+        if(strcmp(nomeTreinador, treinadores[i].nome)==0){
+            tag_achei=1;
+            aux = treinadores[i].pokebola;
+            treinadores[i].pokebola = (Pokemon*) realloc(aux, (treinadores[i].nPokemons+1)*sizeof(Pokemon));
+            if(treinadores[i].pokebola == NULL){
+                printf("Problema de alocacao!\n");
+                for(i=0; i<qtdCompetidores; i++){
+                    free(treinadores[i].pokebola);
+                }
+                free(treinadores);
+                exit(1);
+            }
+            treinadores[i].pokebola[treinadores[i].nPokemons] = pokemon;
+            treinadores[i].nPokemons++;
+        }
+    }
+    if(tag_achei == 0){
+        printf("Esse treinador nao existe!\n");
+        return treinadores;
+    }
+    printf("Pokemon Adicionado com sucesso!\n");
+    return treinadores;
+}
+
+Treinador *removeTreinador(Treinador *treinadores, int *qtdCompetidores, int indexTreinador){
+    Treinador auxTroca;
+    Treinador *auxRealloc;
+    auxTroca = treinadores[indexTreinador];
+    treinadores[indexTreinador] = treinadores[*qtdCompetidores-1];
+    treinadores[*qtdCompetidores-1] = auxTroca;
+    (*qtdCompetidores)--;
+    auxRealloc = treinadores;
+    treinadores = (Treinador *) realloc(auxRealloc, (*qtdCompetidores) * sizeof(Treinador));
+    if(treinadores == NULL){
+        printf("Problema de alocacao!\n");
+        for(int i=0; i<*qtdCompetidores; i++){
+            free(treinadores[i].pokebola);
+        }
+        free(treinadores);
+        exit(1);
+    }
+    return treinadores;
+}
+
+char *batalha(Treinador **treinadores, char *nomeCompetidor1, char *nomeCompetidor2, int indexPokemon1, int indexPokemon2, int *qtdCompetidores){
+    int iCompet1, vantCompet1;
+    int iCompet2, vantCompet2;
+    int i=0;
+    int tag_achei1 = 0;
+    int tag_achei2 = 0;
+    char vencedor[30];
+    for(i=0; i<*qtdCompetidores; i++){
+        if(strcmp((*treinadores)[i].nome, nomeCompetidor1)==0){
+            iCompet1 = i;
+            tag_achei1 = 1;
+        }
+        if(strcmp((*treinadores)[i].nome, nomeCompetidor2)==0){
+            iCompet2 = i;
+            tag_achei2 = 1;
+        }
+    }
+    if(tag_achei1 == 0 || tag_achei2 == 0){
+        printf("Algum dos treinadores nao existe!\n");
+        return "Nao ha vencedor.\n";
+    }
+    if(indexPokemon1 >= (*treinadores)[iCompet1].nPokemons || indexPokemon2 >= (*treinadores)[iCompet2].nPokemons){
+        printf("Algum dos pokemons escolhidos nao existe!\n");
+        return "Nao ha vencedor.\n";
+    }
+    
+    vantCompet1 = (*treinadores)[iCompet1].pokebola[indexPokemon1].ataque - (*treinadores)[iCompet2].pokebola[indexPokemon2].defesa;
+    vantCompet2 = (*treinadores)[iCompet2].pokebola[indexPokemon2].ataque - (*treinadores)[iCompet1].pokebola[indexPokemon1].defesa;
+    
+    if(vantCompet1 > vantCompet2){
+        (*treinadores)[iCompet1].pontuacao++;
+        (*treinadores)[iCompet2].pontuacao--;
+        if((*treinadores)[iCompet2].pontuacao==-5){
+            printf("O treinador %s foi desclassificado da competicao.\n", (*treinadores)[iCompet2].nome);
+            (*treinadores) = removeTreinador(*treinadores, &*qtdCompetidores, iCompet2);
+        }
+        return (*treinadores)[iCompet1].nome;
+    }
+    else if(vantCompet2 > vantCompet1){
+        (*treinadores)[iCompet2].pontuacao++;
+        (*treinadores)[iCompet1].pontuacao--;
+        if((*treinadores)[iCompet1].pontuacao==-5){
+            printf("O treinador %s foi desclassificado da competicao.\n", (*treinadores)[iCompet1].nome);
+            (*treinadores) = removeTreinador(*treinadores, &*qtdCompetidores, iCompet1);
+        }
+        return (*treinadores)[iCompet2].nome;
+    }
 }
