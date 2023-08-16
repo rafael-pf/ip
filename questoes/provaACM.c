468baa75289cc103af7eb1ef9c0be941e62576f3
rafael-pf <132072483+rafael-pf@users.noreply.github.com>
2023-08-15 21:33:02 -0300

    Update provaACM.c

--- questoes/provaACM.c
+++ questoes/provaACM.c
@@ -6,7 +6,7 @@ typedef struct{
     char nome[50];
     char cpf[12];
     int qtdNotas;
-    int *notas;
+    float *notas;
 } Aluno;
 
 Aluno *carregaAlunos(int *qtdAlunos){
@@ -24,35 +24,101 @@ Aluno *carregaAlunos(int *qtdAlunos){
         if(alunos == NULL){printf("Problema de alocacao!\n"); free(aux); exit(1);}
         fscanf(arq, " %49[^\n]", alunos[*qtdAlunos].nome);
         fscanf(arq, " %11[^\n]", alunos[*qtdAlunos].cpf);
+        alunos[*qtdAlunos].qtdNotas = 0;
+        alunos[*qtdAlunos].notas = NULL;
         (*qtdAlunos)++;
     }
     fclose(arq);
     return alunos;
 }
 float *leNotas(int *qtdNotas){
-
+    float *vet=NULL, *aux;
+    float n;
+    printf("Digite as notas (caso deseje parar, digite um numero negativo): \n");
+    do{
+        scanf("%f", &n);
+        if(n>=0){
+            aux = vet;
+            vet = (float *) realloc(aux, ((*qtdNotas)+1)*sizeof(float));
+            if(vet == NULL){
+                printf("Problema de alocacao!\n");
+                free(aux);
+                exit(1);
+            }
+            vet[*qtdNotas] = n;
+            (*qtdNotas)++;
+            printf("Nota cadastrada com sucesso.\n");
+        }
+        else{ printf("Insercao de notas encerrada.\n"); }
+    }while(n>=0);
+    return vet;
 }
 float media(float *notas, int qtdNotas){
-
+    int i;
+    float soma=0;
+    for(i=0; i<qtdNotas; i++){
+        soma+=notas[i];
+    }
+    return (soma/qtdNotas);
 }
 void ordena(Aluno *alunos, int qtdAlunos){
-
+    int i, j;
+    Aluno aux;
+    for(i=0; i<qtdAlunos; i++){
+        for(j=i+1; j<qtdAlunos; j++){
+            if(media(alunos[i].notas, alunos[i].qtdNotas) < media(alunos[j].notas, alunos[j].qtdNotas)){
+                aux = alunos[i];
+                alunos[i] = alunos[j];
+                alunos[j] = aux;
+            }
+        }
+    }
 }
-void salvaAlunos(Aluno *alunos, int qtdAlunos){
 
+void salvaAlunos(Aluno *alunos, int qtdAlunos){
+    FILE *arq;
+    arq = fopen("alunos.bin", "w");
+    if(arq == NULL){
+        printf("Problema ao abrir o arquivo.\n");
+        free(alunos);
+        exit(1);
+    }
+    int i=0;
+    for(i=0; i<qtdAlunos; i++){
+        fwrite(&alunos[i], sizeof(Aluno), 1, arq);
+        /*for(int j=0; j<alunos[i].qtdNotas; j++){
+            fwrite(&alunos[i].notas[j], sizeof(float), 1, arq);
+        }*/
+    }
+
+    fclose(arq);
 }
 
 int main(){
     Aluno *alunos=NULL;
-    float *notas=NULL;
-    int qtdAlunos=0;
+    int qtdAlunos=0, qtdNotas=0;
+    int i=0, j=0;
+
     alunos = carregaAlunos(&qtdAlunos);
     qtdAlunos--;
-    /*for(int i=0; i<qtdAlunos; i++){
-        printf("Nome: %s\nCpf: %s\n", alunos[i].nome, alunos[i].cpf);
-    }*/
     
+    for(i=0; i<qtdAlunos; i++){
+        printf("Cadastrando notas de %s...\n", alunos[i].nome);
+        alunos[i].notas = leNotas(&alunos[i].qtdNotas);
+    }
+    
+    ordena(alunos, qtdAlunos);
 
+    salvaAlunos(alunos, qtdAlunos);
 
+    for(i=0; i<qtdAlunos; i++){
+        printf("Nome: %s\nCpf: %s\n", alunos[i].nome, alunos[i].cpf);
+        printf("Notas: \n");
+        for(j=0; j<alunos[i].qtdNotas; j++){
+            printf("\t%.1f\n", alunos[i].notas[j]);
+        }
+    }
+
+    free(alunos);
     return 0;
 }
